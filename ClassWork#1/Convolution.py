aee0906d940cbc5378c689f11dbc234ef25a82bd
Siriwat Huntra <siriwat26room1@gmail.com>
2023-07-04 10:54:03 +0700

    fix bug

--- ClassWork#1/Convolution.py
+++ ClassWork#1/Convolution.py
@@ -17,6 +17,6 @@ kernel = np.ones((5,5),np.float32)/25
 convoluted_image = convolute(line_image, kernel)
 
 cv2.imshow('image', convoluted_image)
-cv2.imwrite('solution.jpeg', img)
+cv2.imwrite('solution.jpeg', convoluted_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
