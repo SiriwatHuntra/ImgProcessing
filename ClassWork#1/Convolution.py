8ecbadf3ff32d0749b9cd7698932ef6de59c425a
Siriwat Hunntra <siriwat26room1@gmail.com.com>
2023-07-19 09:32:30 +0700

    .

--- ClassWork#1/Convolution.py
+++ ClassWork#1/Convolution.py
@@ -1,4 +1,4 @@
-import cv2
+import cv
 import numpy as np
 
 import numpy as np
@@ -29,23 +29,23 @@ def draw_line(image, start, end, color=255):
 def convolute_image(image, kernel_image):
     kernel_sum = kernel_image.sum()
     kernel = kernel_image.astype(np.float32) / kernel_image.sum()
-    conv = cv2.filter2D(src=image, ddepth=-1, kernel=kernel)
+    conv = cv.filter2D(src=image, ddepth=-1, kernel=kernel)
     return conv
 
-image = cv2.imread('image.png', cv2.IMREAD_GRAYSCALE)
-cv2.imshow('original', image)
+image = cv.imread('image.png', cv.IMREAD_GRAYSCALE)
+cv.imshow('original', image)
 
 line_image = np.zeros((512, 512), dtype='uint8')
 
 start = (10, 10)
 end = (20, 20)
 line_image = draw_line(line_image, start, end)
-cv2.imwrite('line.jpeg', line_image)
-cv2.imshow('line', line_image)
+cv.imwrite('line.jpeg', line_image)
+cv.imshow('line', line_image)
 
 conv = convolute_image(image, line_image)
 
-cv2.imshow('image', conv)
-cv2.imwrite('solution.jpeg', conv)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+cv.imshow('image', conv)
+cv.imwrite('solution.jpeg', conv)
+cv.waitKey(0)
+cv.destroyAllWindows()
